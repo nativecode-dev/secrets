40bdc27a446f2f1b148d95accf45be92f1df8629
Mike Pham <mpham@nativecode.com>
2016-07-09 00:28:49 -0400

    mvc

---
+++ Secrets/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿namespace Secrets.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
